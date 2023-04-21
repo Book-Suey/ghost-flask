9324b5da80a8aecb46841321083b290625668f01
Eric Anderson <eric@booksuey.com>
2023-04-21 13:21:10 -0400

    Added test webhook route

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return "Hello Eric's Ghost!"
+
+@app.route('/ghost-event', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        print('Data received from Webhook is: ', request.json)
+        return "Webhook received!"
